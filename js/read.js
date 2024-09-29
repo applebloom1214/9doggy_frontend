447fcfe93cf9f86ddf03d439375706662c02d20a
applebloom1214 <rainbow610@naver.com>
2024-09-29 19:48:11 +0900

    zzz

---
+++ js/read.js
@@ -0,0 +1,13 @@
+function like() {
+    let like__trigger = false;
+    const read__like = document.querySelector(".read__like");
+
+    if (!like__trigger) {
+        like__trigger = !like__trigger;
+        read__like.style = "red";
+    } else {
+
+    }
+
+
+}
